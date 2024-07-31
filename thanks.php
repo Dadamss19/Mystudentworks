f83186894478e94f46f84ab15df76fa9c48ff5a2
Dadamss19 <monika.adams98@gmail.com>
2024-07-30 21:42:04 -0700

    Add files via upload

--- thanks.php
+++ thanks.php
@@ -1,45 +1,39 @@
-<?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Retrieve form data
-    $name = $_POST["name"];
-    $username = $_POST["username"];
-    $password = $_POST["password"]; // Note: In a real application, never store passwords in plain text
-    $email = $_POST["email"];
-    $play_time = $_POST["play_time"];
-    $favorite_pack = $_POST["favorite_pack"];
-    $feedback = $_POST["feedback"];
-
-    // Validate input
-    if (empty($name) || empty($username) || empty($password) || empty($email) || empty($play_time) || empty($favorite_pack) || empty($feedback)) {
-        die("Error: All fields are required.");
-    }
-
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        die("Error: Invalid email format.");
-    }
-
-    // Process the data (in this example, we'll just write it to a file)
-    $data = "Name: $name\n";
-    $data .= "Username: $username\n";
-    $data .= "Email: $email\n";
-    $data .= "Play Time: $play_time\n";
-    $data .= "Favorite Pack: $favorite_pack\n";
-    $data .= "Feedback: $feedback\n\n";
-
-    $file = fopen("survey_results.txt", "a");
-    fwrite($file, $data);
-    fclose($file);
-
-    // Send a confirmation email
-    $to = $email;
-    $subject = "Thank you for completing the Sims 4 Expansion Packs Survey";
-    $message = "Dear $name,\n\nThank you for participating in our Sims 4 Expansion Packs Survey. We appreciate your feedback!\n\nBest regards,\nThe Survey Team";
-    $headers = "From: noreply@example.com";
-
-    mail($to, $subject, $message, $headers);
-
-    // Redirect to a thank you page
-    header("Location: thank_you.html");
-    exit();
-}
-?>
+<html>
+<head>
+  <title>Form reciept</title>
+  <style>
+  body {background-color:Lightsteelblue; font-family:Calibri;}
+  </style>
+ </head>
+ <body>
+  <p>
+     Welcome <?php echo $_POST["fname"]; ?> <br />
+	 Your last name is <?php echo $_POST["lname"]; ?> <br />
+     Your email address is <?php echo $_POST["email"]; ?> <br />
+     Your password is <?php echo $_POST["password"]; ?> <br />
+     Your response to how long have you been playing is <?php echo $_POST["less than a year"]; ?> <br />
+	 <?php echo $_POST["1 to 3 years"]; ?> <br />
+	  <?php echo $_POST["more than 3 years"]; ?> <br />
+	  <?php echo $_POST["more than 5 years"]; ?> <br />
+	  
+     The expansion packs you picked <?php echo $_POST["Get to Work"]; ?> <br />                                    <?php echo $_POST["Get to Together"]; ?> <br />
+	 <?php echo $_POST["Cats & Dogs"]; ?> <br /> 
+	 <?php echo $_POST["Seasons"]; ?> <br />
+	 <?php echo $_POST["Get Famous"]; ?> <br />
+	 <?php echo $_POST["Island Living"]; ?> <br />
+	 <?php echo $_POST["Discover University"]; ?> <br />
+	 <?php echo $_POST["Eco Lifestyle"]; ?> <br />
+	 <?php echo $_POST["Snowy Escape"]; ?> <br />
+	 <?php echo $_POST["Cottage Living"]; ?> <br />
+	 <?php echo $_POST["Highschool Years"]; ?> <br />
+	 
+	 The feedback you entered <?php echo $_POST["comments"]; ?> <br />
+	 
+	 </p>
+	 
+ 
+	 
+  
+  
+  </body>
+ </html>
